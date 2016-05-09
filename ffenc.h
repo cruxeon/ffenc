2d0de6e4df062401c5dedd6fe0c0f206e642cfbe
Apurva Kumar <cruxeon@users.noreply.github.com>
2016-05-09 04:02:56 -0400

    Create ffenc.h

---
+++ ffenc.h
@@ -0,0 +1,40 @@
+#include <cstring>
+
+extern "C" {
+#include <libavutil/opt.h>
+#include <libavcodec/avcodec.h>
+#include <libavutil/imgutils.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <libavutil/pixfmt.h>
+}
+
+class Encoder//: public IEncoder
+{
+public:
+	Encoder();
+
+	bool initialize();
+	bool configure(int width = 352, int height = 288, int fps = 25);
+	bool encodeFrame(/*RenderTarget* source, */char* sei_data, size_t lenData);
+	bool dataAvailable();
+	bool lockBitstream(const void** stptr, uint32_t* bytes);
+	void unlockBitstream();
+	void shutdown();
+
+private:
+	int frameCount;
+	int got_output;
+
+	char* sei_msg;
+
+	//struct SwsContext* myImgConvertCtx;
+	//PixelData* pixels;
+	//uint8_t* myBuffer;
+
+	AVCodec* codec;
+	AVCodecContext* c;
+	AVFrame *frame;
+	//AVFrame *frameRGB;
+	AVPacket packet;
+};
